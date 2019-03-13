8ec3e115d0d3dd9d6eb8da52eda99299985b3792
ggling <skaty3@163.com>
2019-03-13 20:29:31 +0800

    高 测试01

---
+++ Test01/src/main/java/cn/test/TestCupdata01.java
@@ -0,0 +1,13 @@
+package cn.test;
+
+/**
+ * Administrator
+ * 2019/3/13
+ */
+public class TestCupdata01 {
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
