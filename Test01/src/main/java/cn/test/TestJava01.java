37c1035420ae91e847dec27c5e3b0a4597bd0544
ggling <skaty3@163.com>
2019-03-14 15:07:49 +0800

    高 测试04

--- Test01/src/main/java/cn/test/TestJava01.java
+++ Test01/src/main/java/cn/test/TestJava01.java
@@ -15,21 +15,6 @@ public class TestJava01 {
 
         DecimalFormat format1 = new DecimalFormat("#");
 
-        DecimalFormat df1 = new DecimalFormat("0000.0");
-
-        DecimalFormat df2 = new DecimalFormat("#.#");
-
-        DecimalFormat df3 = new DecimalFormat("000.000");
-
-        System.out.println(df1.format(12.34));
-
-        System.out.println(df2.format(12.34));
-
-        System.out.println(df3.format(12.34));
-        DecimalFormat df4 = new DecimalFormat("#.#0");
-
-
-        System.out.println(df4.format(12.3411));
 
 
     }
